f7ddd84fabb92cc2b7bd2b873551a6f51d258a8b
Brian Parma <brian_parma@partech.com>
2022-12-14 15:38:50 -0700

    add click listener
    add logging

--- app/src/main/java/gov/jeaco/testlv/PageA.kt
+++ app/src/main/java/gov/jeaco/testlv/PageA.kt
@@ -2,13 +2,14 @@ package gov.jeaco.testlv
 
 import kotlinx.coroutines.flow.MutableStateFlow
 import me.tatarka.bindingcollectionadapter2.ItemBinding
+import java.util.function.Consumer
 
 private val logger = mu.KotlinLogging.logger {}
 
 class PageA {
 
     val itemBinding = ItemBinding.of<String>(BR.vm, R.layout.item)
-        .bindExtra(BR.clicked, PageA::clicked)
+        .bindExtra(BR.clicked, Consumer { s: String -> clicked(s) })
 
     val items = MutableStateFlow(emptyList<String>())
 
